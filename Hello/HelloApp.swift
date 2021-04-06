a8aa7add4c315cbc5c96129eb786f050c4cafae9
Vishank Rughwani <vishankr@uw.edu>
2021-04-05 22:18:36 -0700

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Vishank Rughwani on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
